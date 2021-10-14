10f2c9f30e5405097a3c236dbb637b17cf20aebd
Liam Pribis <jackpribis@gmail.com>
2021-10-14 21:52:37 +1300

    bench

---
+++ bench.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ns=(101 201 301 401 501 601 701 801 901)
+t=4
+i=100
+exe=new_world
+
+for n in ${ns[@]}; do
+    echo "running for n=$n"
+    echo "$exe -n $n -t $i -t $t"
+    time ( ./$exe -n $n -t $i -t $t > /dev/null 2>&1 ) 2>&1
+done
